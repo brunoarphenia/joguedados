8efd1db630b8a9c626935a2a4b2445c3cb11c9b4
brunoarphenia <45920547+brunoarphenia@users.noreply.github.com>
2021-04-29 23:53:30 -0300

    animation-add

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 let element = document.getElementById('dado')
+let dado = element.children[0]
+
+
 
 function result() {
 
     let n = Math.round(Math.random() * 6);
+
+    element.classList.toggle('is-open');
 
     console.log(n)
 
     switch (n) {
         case 1:
             element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">'
+            console.log(dado)
             break;
         case 2:
             element.innerHTML = '<img src="./assets/2.svg" alt="Dado - Resultado 2">'
@@ -24,11 +30,13 @@ function result() {
 
             break;
         case 6:
-            element.innerHTML = '<img src="./assets/5.svg" alt="Dado - Resultado 5">'
+            element.innerHTML = '<img src="./assets/5.svg" alt="Dado - Resultado 6">'
             break;
 
         default:
             element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">'
             break;
     }
+
+    
 }
