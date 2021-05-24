9ec9359572a44d3d44fa6b3a5a76e37fc35072cf
brunoarphenia <45920547+brunoarphenia@users.noreply.github.com>
2021-05-24 19:48:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,37 @@
-let element = document.getElementById('dado')
-let dado = element.children[0]
-
-
+let element = document.getElementById("dado");
+let dado = element.children[0];
 
 function result() {
+  let n = Math.floor(Math.random() * 6);
 
-    let n = Math.round(Math.random() * 6);
+  element.classList.toggle("is-open");
 
-    element.classList.toggle('is-open');
+  console.log(n);
 
-    console.log(n)
+  switch (n) {
+    case 1:
+      element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">';
+      console.log(dado);
+      break;
+    case 2:
+      element.innerHTML = '<img src="./assets/2.svg" alt="Dado - Resultado 2">';
+      break;
+    case 3:
+      element.innerHTML = '<img src="./assets/3.svg" alt="Dado - Resultado 3">';
+      break;
+    case 4:
+      element.innerHTML = '<img src="./assets/4.svg" alt="Dado - Resultado 4">';
+      break;
+    case 5:
+      element.innerHTML = '<img src="./assets/5.svg" alt="Dado - Resultado 5">';
 
-    switch (n) {
-        case 1:
-            element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">'
-            console.log(dado)
-            break;
-        case 2:
-            element.innerHTML = '<img src="./assets/2.svg" alt="Dado - Resultado 2">'
-            break;
-        case 3:
-            element.innerHTML = '<img src="./assets/3.svg" alt="Dado - Resultado 3">'
-            break;
-        case 4:
-            element.innerHTML = '<img src="./assets/4.svg" alt="Dado - Resultado 4">'
-            break;
-        case 5:
-            element.innerHTML = '<img src="./assets/5.svg" alt="Dado - Resultado 5">'
+      break;
+    case 6:
+      element.innerHTML = '<img src="./assets/6.svg" alt="Dado - Resultado 6">';
+      break;
 
-            break;
-        case 6:
-            element.innerHTML = '<img src="./assets/5.svg" alt="Dado - Resultado 6">'
-            break;
-
-        default:
-            element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">'
-            break;
-    }
-
-    
+    default:
+      element.innerHTML = '<img src="./assets/1.svg" alt="Dado - Resultado 1">';
+      break;
+  }
 }
